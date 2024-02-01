d3cbdaf0b40f0d17409956f4e8c98c317525d81a
Luiz Seixas <ticoluizgustavo@outlook.com>
2024-02-01 13:33:00 -0300

    feat: game over on touch snake body

--- main.cpp
+++ main.cpp
@@ -116,6 +116,11 @@ void Logic() {
   }
   if (x > width || x < 0 || y > height || y < 0)
     gameOver = true;
+  for (int i = 0; i < nTail; i++) {
+    if (tailX[i] == x && tailY[i] == y)
+      gameOver = true;
+  }
+
   if (x == fruitX && y == fruitY) {
     score += 10;
     fruitX = rand() % width;

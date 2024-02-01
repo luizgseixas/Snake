be617fa62f46d91bcd778fed404db48bcd8a14f0
Luiz Seixas <ticoluizgustavo@outlook.com>
2024-02-01 13:25:35 -0300

    feat: create snake body

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 void Setup() {
@@ -30,19 +32,27 @@ void Draw() {
         for (int j = 0; j < width; j++) {
             if (j == 0)
                 mvprintw(i + 1, j, "#");
-            
             if (i == y && j == x)
                 mvprintw(i + 1, j + 1, "O");
             else if (i == fruitY && j == fruitX)
                 mvprintw(i + 1, j + 1, "F");
-            else
-                mvprintw(i + 1, j + 1, " ");
-
+            else {
+                bool printTail = false;
+                for (int k = 0; k < nTail; k++) {
+                  if (tailX[k] == j && tailY[k] == i) {
+                    int tailPosX = tailX[k] + 1;
+                    int tailPosY = tailY[k] + 1;
+                    mvprintw(tailPosY, tailPosX, "%c", 'o');
+                    printTail = true;
+                  }
+                }
+                if (!printTail)
+                  mvprintw(i + 1, j + 1, " ");
+            }
             if (j == width - 1)
                 mvprintw(i + 1, j + 2, "#");
         }
     }
-
     // Exibir a borda inferior
     for (int i = 0; i < width + 2; i++)
         mvprintw(height + 1, i, "#");
@@ -75,6 +85,19 @@ void Input() {
   }
 }
 void Logic() {
+  int prevX = tailX[0];
+  int prevY = tailY[0];
+  int prev2X, prev2Y;
+  tailX[0] = x;
+	tailY[0] = y;
+  for (int i = 1; i < nTail; i++) {
+    prev2X = tailX[i];
+    prev2Y = tailY[i];
+    tailX[i] = prevX;
+    tailY[i] = prevY;
+    prevX = prev2X;
+    prevY = prev2Y;
+  }
   switch (dir) {
     case LEFT:
       x--;
@@ -97,6 +120,7 @@ void Logic() {
     score += 10;
     fruitX = rand() % width;
     fruitY = rand() % height;
+    nTail++;
   }
 }
 int main() {
